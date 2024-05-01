4222c3881eafb6ba28f9e56c4cdb8152366ef60b
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2024-05-01 22:15:13 +0800

    Create DayEgg.java

---
+++ DayEgg.java
@@ -0,0 +1,85 @@
+package nomor1;
+public class Soal01 {
+    
+    // method 1
+    public void penjumlahan(int a, int b){
+        System.out.println(a + b);
+    }
+    // overloadiang 1
+    public void penjumlahan(double c, double d){
+        System.out.println(c + d);
+    }
+    // method 2
+    public void pengurangan(int e, int f){
+        System.out.println(e - f);
+    }
+    // overloading 2
+    public void pengurangan(double g, double h){
+        System.out.println(g - h);
+    }  
+    // method 3
+    public void pembagian(int i, int j){
+        System.out.println(i / j);
+    }
+    // overloadiang 3
+    public void pembagian(double k, double l){
+        System.out.println(k / l);
+    }
+    // method 4
+    public void perkalian(int m, int n){
+        System.out.println(m * n);
+    }
+    // overloadiang 4
+    public void perkalian(double o, double p){
+        System.out.println(o * p);
+    }
+    // method 5
+    public void modulus(int q, int r){
+        System.out.println(q % r);
+    }
+    // overloading 5
+    public void modulus(double s, double t){
+        System.out.println(s % t);
+    }
+    // method 6
+    public void liverpool(String nama){
+        System.out.println(nama);
+    }
+    // overloading 6
+    public void liverpool(String posisi, String alamat){
+        System.out.println(posisi + " " + alamat);
+    }
+    // method 7
+    public void arsenal(String nama){
+        System.out.println(nama);
+    }
+    // overloading 7
+    public void arsenal(String posisi, String alamat){
+        System.out.println(posisi + " " + alamat);
+    }
+    // method 8
+    public void barcelona(String nama){
+        System.out.println(nama);
+    }
+    // overloading 8
+    public void barcelona(String posisi, String alamat){
+        System.out.println(posisi + " " + alamat);
+    }
+    // method 9
+    public void psg(String nama){
+        System.out.println(nama);
+    }
+    // overloading 9
+    public void psg(String posisi, String alamat){
+        System.out.println(posisi + " " + alamat);
+    }
+    // method 10
+    public void leverkusen(String nama){
+        System.out.println(nama);
+    }
+    // overloading 10
+    public void leverkusen(String posisi, String alamat){
+        System.out.println(posisi + " " + alamat);
+    }
+      
+}
